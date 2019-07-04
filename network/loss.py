0bdf3083def3714ac280cbbf9d447c70f426a347
jeonminkyu <jskstar12@naver.com>
2019-07-04 23:09:43 +0900

    edit nn -> F(relu in D_loss)

--- network/loss.py
+++ network/loss.py
@@ -66,4 +66,4 @@ class LossD(nn.Module):
         self.zero = torch.zeros([1]).type(self.dtype)
 
     def forward(self, r_x, r_x_hat):
-        return torch.nn.ReLU(1 + r_x_hat[0]) + torch.nn.ReLU(1 - r_x[0])
+        return F.relu(1 + r_x_hat[0]) + F.relu(1 - r_x[0])
