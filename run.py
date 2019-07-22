538824838e9945b80d2de79cecd0a41f0f143c4b
MrCaracara <lucas@grey-eye.net>
2019-07-22 17:52:41 +0300

    Training may now be done in batches and using several GPU's

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 import numpy as np
 import torch
 from PIL import Image
+from torch.nn import DataParallel
 from torch.optim import Adam
 from torchvision import transforms
 from torch.utils.data import DataLoader
@@ -18,16 +19,23 @@ from dataset import preprocess_dataset, VoxCelebDataset
 import matplotlib.pyplot as plt
 
 
-def meta_train(device, dataset_path, continue_id):
+class ParallelDiscriminator(DataParallel):
+    @property
+    def W(self):
+        return self.module.W
+
+
+def meta_train(gpu, dataset_path, continue_id):
     run_start = datetime.now()
     logging.info('===== META-TRAINING =====')
     # GPU / CPU --------------------------------------------------------------------------------------------------------
-    if device is not None and device != 'cpu':
+    if gpu:
         dtype = torch.cuda.FloatTensor
-        torch.cuda.set_device(device)
+        torch.set_default_tensor_type(dtype)
         logging.info(f'Running on GPU: {torch.cuda.current_device()}.')
     else:
         dtype = torch.FloatTensor
+        torch.set_default_tensor_type(dtype)
         logging.info(f'Running on CPU.')
 
     # DATASET-----------------------------------------------------------------------------------------------------------
@@ -35,7 +43,6 @@ def meta_train(device, dataset_path, continue_id):
     dataset = VoxCelebDataset(
         root=dataset_path,
         extension='.vid',
-        shuffle=False,
         shuffle_frames=True,
         transform=transforms.Compose([
             transforms.Resize(config.IMAGE_SIZE),
@@ -44,18 +51,13 @@ def meta_train(device, dataset_path, continue_id):
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ])
     )
-    dataset = DataLoader(dataset, batch_size=config.BATCH_SIZE, num_workers=0)
+    dataset = DataLoader(dataset, batch_size=config.BATCH_SIZE, shuffle=True)
 
     # NETWORK ----------------------------------------------------------------------------------------------------------
 
     E = network.Embedder().type(dtype)
     G = network.Generator().type(dtype)
-    D = network.Discriminator(143000).type(dtype)
-
-    if continue_id is not None:
-        E = load_model(E, continue_id)
-        G = load_model(G, continue_id)
-        D = load_model(D, continue_id)
+    D = network.Discriminator(len(dataset)).type(dtype)
 
     optimizer_E_G = Adam(
         params=list(E.parameters()) + list(G.parameters()),
@@ -66,11 +68,26 @@ def meta_train(device, dataset_path, continue_id):
         lr=config.LEARNING_RATE_D
     )
 
-    criterion_E_G = network.LossEG(device, feed_forward=True)
-    criterion_D = network.LossD(device)
+    criterion_E_G = network.LossEG(feed_forward=True)
+    criterion_D = network.LossD()
+
+    if gpu:
+        E = DataParallel(E)
+        G = DataParallel(G)
+        D = ParallelDiscriminator(D)
+        criterion_E_G = DataParallel(criterion_E_G)
+        criterion_D = DataParallel(criterion_D)
+
+    if continue_id is not None:
+        E = load_model(E, 'Embedder', continue_id)
+        G = load_model(G, 'Generator', continue_id)
+        D = load_model(D, 'Discriminator', continue_id)
 
     # TRAINING LOOP ----------------------------------------------------------------------------------------------------
-    logging.info(f'Starting training loop. Epochs: {config.EPOCHS} Dataset Size: {len(dataset)}')
+    logging.info(f'Starting training loop. '
+                 f'Epochs: {config.EPOCHS} '
+                 f'Batches: {len(dataset)} '
+                 f'Batch Size: {config.BATCH_SIZE}')
 
     for epoch in range(config.EPOCHS):
         epoch_start = datetime.now()
@@ -106,8 +123,8 @@ def meta_train(device, dataset_path, continue_id):
             optimizer_E_G.zero_grad()
             optimizer_D.zero_grad()
 
-            loss_E_G = criterion_E_G(x_t, x_hat, r_x_hat, e_hat, D.W[:, i], D_act, D_act_hat)
-            loss_D = criterion_D(r_x, r_x_hat)
+            loss_E_G = criterion_E_G(x_t, x_hat, r_x_hat, e_hat, D.W[:, i], D_act, D_act_hat).mean()
+            loss_D = criterion_D(r_x, r_x_hat).mean()
             loss = loss_E_G + loss_D
             loss.backward()
 
@@ -120,62 +137,62 @@ def meta_train(device, dataset_path, continue_id):
             r_x, D_act = D(x_t, y_t, i)
 
             optimizer_D.zero_grad()
-            loss_D = criterion_D(r_x, r_x_hat)
+            loss_D = criterion_D(r_x, r_x_hat).mean()
             loss_D.backward()
             optimizer_D.step()
 
             batch_end = datetime.now()
             batch_durations.append(batch_end - batch_start)
             # SHOW PROGRESS --------------------------------------------------------------------------------------------
-            if (batch_num + 1) % 100 == 0 or batch_num == 0:
+            if (batch_num + 1) % 1 == 0 or batch_num == 0:
                 avg_time = sum(batch_durations, timedelta(0)) / len(batch_durations)
-                logging.info(f'Epoch {epoch+1}: [{batch_num + 1}/{len(dataset)}] | '
+                logging.info(f'Epoch {epoch + 1}: [{batch_num + 1}/{len(dataset)}] | '
                              f'Avg Time: {avg_time} | '
                              f'Loss_E_G = {loss_E_G.item():.4} Loss_D {loss_D.item():.4}')
-                logging.debug(f'D(x) = {r_x.item():.4} D(x_hat) = {r_x_hat.item():.4}')
+                logging.debug(f'D(x) = {r_x.mean().item():.4} D(x_hat) = {r_x_hat.mean().item():.4}')
 
             # SAVE IMAGES ----------------------------------------------------------------------------------------------
-            if (batch_num + 1) % 100 == 0:
+            if (batch_num + 1) % 1 == 0:
                 if not os.path.isdir(config.GENERATED_DIR):
                     os.makedirs(config.GENERATED_DIR)
 
-                save_image(os.path.join(config.GENERATED_DIR, f'{datetime.now():%Y%m%d_%H%M}_x.png'), x_t)
-                save_image(os.path.join(config.GENERATED_DIR, f'{datetime.now():%Y%m%d_%H%M}_x_hat.png'), x_hat)
+                save_image(os.path.join(config.GENERATED_DIR, f'{datetime.now():%Y%m%d_%H%M%S%f}_x.png'), x_t[0])
+                save_image(os.path.join(config.GENERATED_DIR, f'{datetime.now():%Y%m%d_%H%M%S%f}_x_hat.png'), x_hat[0])
 
-            if (batch_num + 1) % 2000 == 0:
-                save_model(E, device)
-                save_model(G, device)
-                save_model(D, device)
+            if (batch_num + 1) % 100 == 0:
+                save_model(E, 'Embedder', gpu)
+                save_model(G, 'Generator', gpu)
+                save_model(D, 'Discriminator', gpu)
 
         # SAVE MODELS --------------------------------------------------------------------------------------------------
 
-        save_model(E, device, run_start)
-        save_model(G, device, run_start)
-        save_model(D, device, run_start)
+        save_model(E, 'Embedder', gpu, run_start)
+        save_model(G, 'Generator', gpu, run_start)
+        save_model(D, 'Discriminator', gpu, run_start)
         epoch_end = datetime.now()
-        logging.info(f'Epoch {epoch+1} finished in {epoch_end - epoch_start}. '
+        logging.info(f'Epoch {epoch + 1} finished in {epoch_end - epoch_start}. '
                      f'Average batch time: {sum(batch_durations, timedelta(0)) / len(batch_durations)}')
 
 
-def save_model(model, gpu, time_for_name=None):
+def save_model(model, name, gpu, time_for_name=None):
     if time_for_name is None:
         time_for_name = datetime.now()
 
     model.eval()
 
-    if gpu is not None:
+    if gpu:
         model.cpu()
 
     if not os.path.exists(config.MODELS_DIR):
         os.makedirs(config.MODELS_DIR)
 
-    filename = f'{type(model).__name__}_{time_for_name:%Y%m%d_%H%M}.pth'
+    filename = f'{name}_{time_for_name:%Y%m%d_%H%M}.pth'
     torch.save(
         model.state_dict(),
         os.path.join(config.MODELS_DIR, filename)
     )
 
-    if gpu is not None:
+    if gpu:
         model.cuda()
 
     logging.info(f'Model saved: {filename}')
@@ -183,12 +200,11 @@ def save_model(model, gpu, time_for_name=None):
     model.train()
 
 
-def load_model(model, continue_id):
-    filename = f'{type(model).__name__}_{continue_id}.pth'
+def load_model(model, name, continue_id):
+    filename = f'{name}_{continue_id}.pth'
     model.load_state_dict(
         torch.load(
-            os.path.join(config.MODELS_DIR,
-                         filename)
+            os.path.join(config.MODELS_DIR, filename)
         )
     )
     return model
@@ -200,7 +216,7 @@ def save_image(filename, data):
     std = np.array([0.229, 0.224, 0.225]).reshape((3, 1, 1))
     mean = np.array([0.485, 0.456, 0.406]).reshape((3, 1, 1))
     img = data.numpy()
-    img = ((img * std + mean).transpose(1, 2, 0)*255.0).clip(0, 255).astype("uint8")
+    img = ((img * std + mean).transpose(1, 2, 0) * 255.0).clip(0, 255).astype("uint8")
     img = Image.fromarray(img)
     img.save(filename)
 
@@ -229,8 +245,8 @@ def main():
                                 help="Path to the folder where the pre-processed dataset will be stored.")
     dataset_parser.add_argument("--size", type=int, default=0,
                                 help="Number of videos from the dataset to process.")
-    dataset_parser.add_argument("--ngpu", type=int, default=0,
-                                help="Number of GPUs available. Ignore for CPU mode.")
+    dataset_parser.add_argument("--gpu", action="store_true",
+                                help="Run the model on GPU.")
     dataset_parser.add_argument("--overwrite", action="store_true",
                                 help="Add this flag to overwrite already pre-processed files. The default functionality"
                                      "is to ignore videos that have already been pre-processed.")
@@ -239,10 +255,10 @@ def main():
     train_parser = subparsers.add_parser("meta-train", help="Starts the meta-training process.")
     train_parser.add_argument("--dataset", type=str, required=True,
                               help="Path to the pre-processed dataset.")
-    train_parser.add_argument("--ngpu", type=int, default=0,
-                              help="Number of GPUs available. Ignore for CPU mode.")
+    train_parser.add_argument("--gpu", action="store_true",
+                              help="Run the model on GPU.")
     train_parser.add_argument("--continue_id", type=str, default=None,
-                                help="Id of the models to continue training.")
+                              help="Id of the models to continue training.")
 
     args = parser.parse_args()
 
@@ -264,14 +280,14 @@ def main():
         if args.subcommand == "meta-train":
             meta_train(
                 dataset_path=args.dataset,
-                device='cuda:0' if (torch.cuda.is_available() and args.ngpu > 0) else 'cpu',
+                gpu=(torch.cuda.is_available() and args.gpu),
                 continue_id=args.continue_id,
             )
         elif args.subcommand == "dataset":
             preprocess_dataset(
                 source=args.source,
                 output=args.output,
-                device='cuda' if (torch.cuda.is_available() and args.ngpu > 0) else 'cpu',
+                device='cuda' if (torch.cuda.is_available() and args.gpu) else 'cpu',
                 size=args.size,
                 overwrite=args.overwrite,
             )
